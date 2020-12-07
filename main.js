a6bd6e509e020917c668fd7b9af7b1f24f6548a2
Eunbyul na Proctor <51739602+EunbyulNa@users.noreply.github.com>
2020-12-07 12:09:22 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
   $( document ).ready( function() {
     var jbTime = 900;
-    $( '.logo' ).animate( {opacity: '1'}, jbTime, function() {
+    $( '.logo > img' ).animate( {opacity: '1', paddingTop: '0'}, jbTime, function() {
       $('.hello').animate( {opacity: '1', marginTop : '0'}, jbTime, function () {
         $('.name').animate( {opacity: '1', marginTop: '0'})
       })
